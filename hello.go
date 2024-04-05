235e50904210b17e68b75a46d2856cfebbd59f37
Soma Sekine <a8121151@aoyama.jp>
2024-04-05 01:02:00 -0400

    switch

--- hello.go
+++ hello.go
@@ -7,12 +7,21 @@ import (
 )
 
 func main() {
-	x := hello.Input("type a price")
-	n, err := strconv.Atoi(x)
-	if err != nil {
-		fmt.Println("ERROR!")
-		return
+	x := hello.Input("type a number")
+	fmt.Print(x + "月は、")
+	switch n, err := strconv.Atoi(x); n {
+	case 0:
+		fmt.Println("整数値が得られません")
+		fmt.Println(err)
+	case 1, 2, 12:
+		fmt.Println("冬です")
+	case 3, 4, 5:
+		fmt.Println("春です")
+	case 6, 7, 8:
+		fmt.Println("夏です")
+	case 9, 10, 11:
+		fmt.Println("冬です")
+	default:
+		fmt.Println("月の値ではありません")
 	}
-	p := float64(n)
-	fmt.Println(int(p * 1.1))
 }
