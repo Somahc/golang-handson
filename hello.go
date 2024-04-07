faa927418ed6d4c8d649cc651ceef6ad97e17cbf
Soma Sekine <a8121151@aoyama.jp>
2024-04-07 00:57:52 -0400

    エントリーでの入力

--- hello.go
+++ hello.go
@@ -11,19 +11,33 @@ import (
 )
 
 func main() {
-	c := 0
+	// c := 0
 
 	a := app.New()
 
 	w := a.NewWindow("Hello")
 	l := widget.NewLabel("Hello Fyne!")
+	e := widget.NewEntry()
+	e.SetText("0")
 	text1 := canvas.NewText("Hello Fyne!", color.White)
 	text2 := canvas.NewText("This is a sample application!", color.White)
-	content := container.NewVBox(text1, text2, l, widget.NewButton("Click me!", func() {
-		c++
-		l.SetText("count: " + strconv.Itoa(c))
-	}))
+	content := container.NewVBox(text1, text2, l, e,
+		widget.NewButton("Click me!", func() {
+			n, _ := strconv.Atoi(e.Text)
+			l.SetText("Total: " + strconv.Itoa(total(n)))
+			// c++
+			// l.SetText("count: " + strconv.Itoa(c))
+		}))
 	w.SetContent(content)
 
+	// a.Settings().SetTheme(&forcedVariant{Theme: theme.DefaultTheme(), variant: theme.VariantDark})
 	w.ShowAndRun()
+}
+
+func total(n int) int {
+	t := 0
+	for i := 1; i <= n; i++ {
+		t += i
+	}
+	return t
 }
