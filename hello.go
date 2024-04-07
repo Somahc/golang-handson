444409b2eeb3fd4cb7228816ea7610cac8c4d3dc
Soma Sekine <a8121151@aoyama.jp>
2024-04-07 00:09:14 -0400

    Fyne正常に動作

--- hello.go
+++ hello.go
@@ -1,27 +1,15 @@
 package main
 
 import (
-	"fmt"
-	"go-handson/hello"
-	"strconv"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
-	x := hello.Input("type a number")
-	fmt.Print(x + "月は、")
-	switch n, err := strconv.Atoi(x); n {
-	case 0:
-		fmt.Println("整数値が得られません")
-		fmt.Println(err)
-	case 1, 2, 12:
-		fmt.Println("冬です")
-	case 3, 4, 5:
-		fmt.Println("春です")
-	case 6, 7, 8:
-		fmt.Println("夏です")
-	case 9, 10, 11:
-		fmt.Println("冬です")
-	default:
-		fmt.Println("月の値ではありません")
-	}
+	a := app.New()
+
+	w := a.NewWindow("Hello")
+	w.SetContent(widget.NewLabel("Hello Fyne!"))
+
+	w.ShowAndRun()
 }
